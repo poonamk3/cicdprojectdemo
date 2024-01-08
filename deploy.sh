6711aed3bb26f785c5c393df2a7550fc464875c1
poonamk3 <poonamk@thoughtwin.com>
2024-01-08 17:47:38 +0530

    change push

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-echo "Deploying to production..."
+
+# echo "Deploying to production..."
 # Add your deployment commands here
 python manage.py makemigrations
 python manage.py migrate
