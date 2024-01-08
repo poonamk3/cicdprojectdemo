4fb3c866788cfb91cbd9cdcdefa7ce71d647d30b
poonamk3 <poonamk@thoughtwin.com>
2024-01-08 16:10:08 +0530

    css

--- cicdproject/settings.py
+++ cicdproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--thpesov^2+w7w6=6@m-*_!)#_b@_m$r3hwc%vbj^xfssu*!%&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
