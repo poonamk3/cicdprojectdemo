7c1ec76efa9acd49de88f711ae2fdb2857f90dc7
poonamk3 <poonamk@thoughtwin.com>
2024-01-08 16:33:01 +0530

    update

--- cicdapp/admin.py
+++ cicdapp/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Student,Student_data
+from .models import Student,Student_data,Emp
 # Register your models here.Student_data
 admin.site.register(Student)
 admin.site.register(Student_data)
+admin.site.register(Emp)
