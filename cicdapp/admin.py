024c4976288dda5bd8e59a99fe00353643d5aa9e
poonamk3 <poonamk@thoughtwin.com>
2024-01-08 17:33:53 +0530

    change push

--- cicdapp/admin.py
+++ cicdapp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Student,Student_data,Emp,Emp_data,cccc,aaa
+from .models import Student,Student_data,Emp,Emp_data,cccc,aaa,dddd
 # Register your models here.Student_data
 admin.site.register(Student)
 admin.site.register(Student_data)
@@ -7,4 +7,5 @@ admin.site.register(Emp)
 admin.site.register(Emp_data)
 admin.site.register(cccc)
 admin.site.register(aaa)
+admin.site.register(dddd)
 
