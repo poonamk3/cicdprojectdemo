943dc7766a61224c3fbef68ebfbe300fa6f3dd1f
poonamk3 <poonamk@thoughtwin.com>
2024-01-08 18:08:29 +0530

    change push

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 
 # echo "Deploying to production..."
 # Add your deployment commands here
-# python manage.py makemigrations
+python manage.py makemigrations
 python manage.py migrate
 # Create a directory named "poonam"
 
