c7a348120febd760ce0200f21e5f3ce0427632d6
poonamk3 <poonamk@thoughtwin.com>
2024-01-16 16:49:10 +0530

    add api

--- cicdapp/urls.py
+++ cicdapp/urls.py
@@ -1,10 +1,12 @@
 # cicd/urls.py
 from django.urls import path
-from .views import cicd_home,render_example
+from .views import cicd_home,render_example,get_result
+
 
 urlpatterns = [
     path('', cicd_home, name='cicd_home'),
     path('render_example/', render_example, name='render_example'),
+    path('api/get_result', get_result, name='get_result'),
     # path('example/', example, name='example'),
     # Add more URL patterns as needed
 ]
--- cicdapp/views.py
+++ cicdapp/views.py
@@ -1,6 +1,9 @@
 # cicd/views.py
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import JsonResponse
+import time
+
 
 def cicd_home(request):
     return HttpResponse(" app! ")
@@ -14,3 +17,15 @@ def render_example(request):
 #     return HttpResponse("Welcome to the CICD app!")
     # context = {'message': 'Hello from the render example!'}
     # return render(request, 'cicd/render_examples.html', context)
+
+from django.http import JsonResponse
+import time
+
+def perform_computation():
+    # Simulate a long computation (10 seconds delay)
+    time.sleep(10)
+    return {"result": "Big Time Result!"}
+
+def get_result(request):
+    result = perform_computation()
+    return JsonResponse(result)
